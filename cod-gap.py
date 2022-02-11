7e4634c79abf924960a64e00b22a17b4969e0cc3
arthur0010 <94835248+arthur0010@users.noreply.github.com>
2022-02-11 23:32:29 +0330

    Create cod-gap.py

---
+++ cod-gap.py
@@ -0,0 +1,46 @@
+import random
+from random import randint
+import string
+import uuid
+import os
+import time 
+from time import sleep
+from colorama import Fore
+import sys 
+print("")
+print(Fore.RED+" "+" Loading....")
+sleep(1.0)
+print("\n"*49)
+loo = """
+     Was connected """
+for i in loo:
+   sleep(0.02)
+print("""
+	
+	  <\>   aqa amir 
+	      
+	    <\>   keristofer
+	        
+	      <\>   [ pedar filtering ]
+	      
+	        <\>   chanel rubika = @aqa_keristofer
+	        
+	        
+	           """)
+print(i,end='',flush=True)
+print()
+sleep(0.9)
+print(Fore.GREEN+" "+"["+"_"*40+"]")
+print(" "+Fore.YELLOW+'cod fil Gap >> ',uuid.uuid1())
+print(" "+Fore.YELLOW+'cod fil Gap >> ',uuid.uuid4())
+print(Fore.GREEN+" "+"["+"_"*40+"]")
+print("""
+	
+	
+	      abar script cod filteri 《 gap 》
+	      
+	      
+	      """) 
+print()
+input()
+    
